d5149da07c599476fef44e386ed032cc036c6631
许西 <xuxi@xuxideMacBook-Pro.local>
2021-03-18 16:43:32 +0800

    publicPath

--- vue.config.js
+++ vue.config.js
@@ -16,7 +16,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: "/",
+  publicPath: "/codex/",
   outputDir: "dist",
   assetsDir: "static",
   lintOnSave: false, // 关闭eslint
